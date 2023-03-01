65bb6abd1c83d2eac0cb289a65cef62fad7cbcdb
Bohdan M <bmv@ik.me>
2023-03-01 16:28:05 +0100

    update task 4

--- src/FourthTask.java
+++ src/FourthTask.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class FourthTask {
     public static void main(String[] args) {
@@ -9,10 +9,11 @@ public class FourthTask {
         System.out.print("Введіть символ: ");
         char ch = input.next().toLowerCase().charAt(0); // переводимо символ до нижнього регістру
 
-        String[] words = text.split("\\W+"); // розділяємо текст на слова
+        String[] words = text.split("[\\s,.!?]+"); // розділяємо текст на слова
 
         // знаходимо максимальну кількість входжень символу в слова
         int maxCount = 0;
+        List<String> maxCountWords = new ArrayList<>();
         for (String word : words) {
             int count = 0;
             for (int i = 0; i < word.length(); i++) {
@@ -22,21 +23,14 @@ public class FourthTask {
             }
             if (count > maxCount) {
                 maxCount = count;
+                maxCountWords.clear(); // очищуємо список слів з максимальною кількістю входжень символу
+            }
+            if (count == maxCount) {
+                maxCountWords.add(word); // додаємо слово до списку, якщо містить символ максимальну кількість разів
             }
         }
 
-        // друкуємо всі слова, що містять символ максимальну кількість разів
-        System.out.print("Слова, що містять символ '" + ch + "' " + maxCount + " разів: ");
-        for (String word : words) {
-            int count = 0;
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == ch) {
-                    count++;
-                }
-            }
-            if (count == maxCount) {
-                System.out.print(word + " ");
-            }
-        }
+        System.out.println("Слова, що містять символ '" + ch + "' " + maxCount + " разів: " + maxCountWords);
     }
 }
+
