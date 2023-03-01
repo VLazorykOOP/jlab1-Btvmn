772973e587564c82638b4249329fe55129d0f5e2
Bohdan M <bmv@ik.me>
2023-03-01 04:27:36 +0100

    the second part of the task, Evaluating an expression for integers with conversion to real numbers, has been completed

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -12,5 +12,16 @@ public class FirstTask {
         double b = scanner.nextDouble();
         double result1 = a / (Math.pow(b, 2) - 2) + b / (Math.pow(a, 2) + 2);
         System.out.println("Result: " + result1);
+
+        // Обчислення виразу для цілих чисел з перетворенням в дійсні числа
+        System.out.println("2. Convert int to double and calculate");
+        System.out.print("Enter a: ");
+        int intA = scanner.nextInt();
+        System.out.print("Enter b: ");
+        int intB = scanner.nextInt();
+        double doubleA = (double) intA; // перетворення цілого числа у дійсне
+        double doubleB = (double) intB;
+        double result2 = doubleA / (Math.pow(doubleB, 2) - 2) + doubleB / (Math.pow(doubleA, 2) + 2);
+        System.out.println("Result: " + result2);
     }
 }
